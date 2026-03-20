47dd0d283310d01167dae7b518ff5a476e431e39
Emeka Allison <emekaallison4@gmail.com>
2026-03-20 08:08:54 +0100

    Use Keccak256 for hashing

--- src/hash_func.rs
+++ src/hash_func.rs
@@ -1,10 +1,13 @@
-use sha2::{Digest, Sha256}; // Ethereum uses Keccak256 for hashing // Ethereum uses Keccak256 for hashing
+use hex; // For converting hash bytes to hexadecimal string
+use sha3::{Digest, Keccak256}; // Ethereum uses Keccak256 for hashing 
 
 pub fn hash(data: &str) -> String {
-    let mut hasher = Sha256::new();
-    hasher.update(data.as_bytes());
+    let mut hasher = Keccak256::new();
+    hasher.update(data);
     let result = hasher.finalize();
-    format!("{:x}", result)
+    // Convert the hash result to a hexadecimal string
+    let hex_result = hex::encode(result);
+    hex_result
 }
 
 pub fn hash_pair(left: &str, right: &str) -> String {
